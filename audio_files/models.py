0e753b4f4718e7be59a789057b67f663d799fdc1
187ram <187ram@gmail.com>
2021-05-09 19:24:54 +0530

    changes model fields

--- audio_files/models.py
+++ audio_files/models.py
@@ -43,7 +43,7 @@ class Podcast(models.Model):
     duration = models.DurationField(validators =[duration_validation])
     uploaded_time = models.DateTimeField(auto_now=True)
     host = models.CharField(max_length=100,)
-    participants = models.CharField(max_length=1000, validators=[participant_validation])
+    participants = models.CharField(max_length=1100, validators=[participant_validation])
     
     class Meta:
         ordering = ["id"]
